b3043efd6ab558fc5a3f40f2b4440467b47c3dbe
auPhippsCSUMB <auphipps@csumb.edu>
2026-04-23 17:17:12 -0700

    api is connected

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import mysql from 'mysql2/promise';
 const app = express();
@@ -18,6 +19,24 @@ const pool = mysql.createPool({
 app.get('/', (req, res) => {
     res.send('Hello Express app!')
 });
+
+app.get('/apiTest', async (req, res) => {
+    let client_id = "3zzzpcewhkvs4yvk9v2117i0xqqviq";
+    let client_secret = "nks9vjjfiq7g0ql2gyf6wjf4r29sry";
+    // grant_type = client_credentials
+
+    let url = "https://id.twitch.tv/oauth2/token";
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`
+    });
+
+    const test = await response.json();
+    console.log(test);
+    res.render('apiTest.ejs', { test });
+});
+
 app.get("/dbTest", async (req, res) => {
     try {
         const [rows] = await pool.query("SELECT CURDATE()");
