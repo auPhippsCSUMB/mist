ecf07b511868b88ecec539bf083cb788a868d144
auPhippsCSUMB <auphipps@csumb.edu>
2026-04-28 14:06:35 -0700

    successfully received information about games from api call

--- index.mjs
+++ index.mjs
@@ -8,13 +8,16 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 //setting up database connection pool, replace values in red
 const pool = mysql.createPool({
-    host: "your_hostname",
-    user: "your_username",
-    password: "your_password",
-    database: "your_database",
+    host: "x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "raf75r08n7zd5356",
+    password: "zjcnac7j466tft93", //put into env file
+    database: "aim59v409g7thmo2",
     connectionLimit: 10,
     waitForConnections: true
 });
+
+let token;
+
 //routes
 app.get('/', (req, res) => {
     res.send('Hello Express app!')
@@ -33,8 +36,28 @@ app.get('/apiTest', async (req, res) => {
     });
 
     const test = await response.json();
+    token = test.access_token;
     console.log(test);
+    console.log('test' + test.toString());
     res.render('apiTest.ejs', { test });
+});
+
+
+app.get('/gameTest', async (req, res) => {
+    let client_id = "3zzzpcewhkvs4yvk9v2117i0xqqviq";
+    let client_secret = "nks9vjjfiq7g0ql2gyf6wjf4r29sry";
+    // grant_type = client_credentials
+
+    let url = "https://api.igdb.com/v4/games";
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Client-ID": process.env.CLIENT_ID, "Authorization": "Bearer " + token },
+        body: "fields *;"
+    });
+
+    const game = await response.json();
+    console.log(game);
+    res.send(game);
 });
 
 app.get("/dbTest", async (req, res) => {
